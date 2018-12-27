06f792c671cd64ee3cc2034011a804738ad64d71
Gregor Ziegltrum <gregi11@web.de>
2018-12-27 19:42:44 +0100

    Fixxed Layout Threre are some Problems with rotation

--- app/src/main/java/com/example/gregor/wavesimulator/MainActivity.java
+++ app/src/main/java/com/example/gregor/wavesimulator/MainActivity.java
@@ -2,12 +2,67 @@ package com.example.gregor.wavesimulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WaveView waveView;
+    private Switch boundarySwitch;
+    private Thread simulation_thread;
+    private int simulation_steps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //section BoundarySwitch init
+        boundarySwitch = findViewById(R.id.switch1);
+        boundarySwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onTouchEventSwitch(v);
+            }
+        });//Defines the onClick  Listener for the switch
+        //waveView init
+        waveView = findViewById(R.id.waveView);
+        waveView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onTouchEventViewer(v);
+            }
+        });//Defines the onClick  Listener for the WaveView
+    }
+    public void onTouchEventViewer(View v)
+    {
+        if(simulation_steps == 0) {
+            simulation_steps = 50;
+            simulation_thread = new Thread(new Runnable() {
+                public void run() {
+                    for (int i = 0; i <simulation_steps; i++) {
+                        waveView.simulate_step();
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                waveView.invalidate();
+                            }
+
+                        });
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            simulation_thread.start();
+        }
+        else
+        {
+            simulation_steps = 0;
+
+        }
+    }
+    public void onTouchEventSwitch (View v)
+    {
+
     }
 }
--- app/src/main/java/com/example/gregor/wavesimulator/WaveView.java
+++ app/src/main/java/com/example/gregor/wavesimulator/WaveView.java
@@ -10,31 +10,35 @@ import android.view.MotionEvent;
 
 
 import Solver.CPPSimulator;
-import Solver.WaveSimulator;
 
 public class WaveView extends View {
-    private boolean touched = false;
-    private int width;
-    private int height;
-    private CPPSimulator sim;
-    Paint paint;
+    private static final int cell_count = 100; //Todo: implement cell_count everywhere (eg in native code)
+    private static int width;
+    private static int height;
+    private static CPPSimulator sim; //have to be static to allow screen rotation
+    private Paint paint;
     public WaveView(Context context,AttributeSet attrs) {
         super(context,attrs);
         width = getWidth();
         height = getHeight();
         paint = new Paint();
-        sim = new CPPSimulator();
-
+        if(sim == null) {
+            sim = new CPPSimulator();
+        }
     }
-    @Override
+   /* @Override
     public boolean onTouchEvent (MotionEvent event)
     {
         sim.simulatetimestep();
         invalidate();//used to trigger redraw
         return true;
+    }*/
+    public void simulate_step()
+    {
+        sim.simulatetimestep();
     }
     @Override
-    protected void onSizeChanged (int w, int h, int oldw, int oldh)
+    protected void onSizeChanged (int w, int h, int old_w, int old_h)
     {
         width = w;
         height = h;
@@ -42,13 +46,18 @@ public class WaveView extends View {
     @Override
     public void onDraw(Canvas canvas)
     {
-       for(int i = 0; i < 100; i++)
+        int size_x = width/ cell_count;
+        int offset_x = (width - size_x * cell_count)/2;
+        int size_y = height/ cell_count;
+        int offset_y = (height - size_y * cell_count)/2;
+       for(int i = 0; i < cell_count; i++)
         {
-            for(int j = 0; j <100; j++)
+            for(int j = 0; j <cell_count; j++)
             {
-                int current = (int)sim.getHeight(i,j)*10;
+                int current = (int)sim.getHeight(i,j)*10; //mal 10 weil wir 100 zellen haben, die domain des szenarios jedoch 1000 ist
                 paint.setColor(Color.rgb(current,0,0));
-                canvas.drawRect(i*10,j*10,(i+1)*10,(j+1)*10,paint);
+
+                canvas.drawRect(offset_x + i*size_x,offset_y +j*size_y,offset_x +(i+1)*size_x,offset_y +(j+1)*size_y,paint);
             }
         }
     }
